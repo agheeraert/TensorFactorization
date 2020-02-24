a0d7ff1259095625e81ccdff3c9a74eab7ac9bf8
Aria GHEERAERT <aria.gheeraert@gmail.com>
2020-02-24 15:27:51 +0100

    corrected plot from npy script

--- plot_cc_from_npy.py
+++ plot_cc_from_npy.py
@@ -17,11 +17,11 @@ for fichier in args.f:
         output = fichier+'.png'
     best5 = np.sort(table, axis=-1)[:,-5:]
     f = plt.figure()
-    plt.plot(range(2,13), np.mean(best5, axis=-1), color='k')
+    plt.plot(range(2,best5.shape[0]+2), np.mean(best5, axis=-1), color='k')
     for i in range(5):
-        plt.scatter(range(2,13), best5[:,i], marker='+', color='g')
+        plt.scatter(range(2,best5.shape[0]+2), best5[:,i], marker='+', color='g')
     plt.xlabel("Number of components")
     plt.ylabel("core consistency (%)")
     plt.ylim(0,100)
-    plt.savefig("results/coreconsistency.png")
+    plt.savefig(output)
 
