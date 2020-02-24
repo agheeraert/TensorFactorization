362dcbb25ec62a0d873b384a8c7b783598d76e7c
Aria GHEEREART <aria.gheeraert@gmail.com>
2020-02-24 15:44:08 +0100

    new separation of pickle

--- separated_pickle.py
+++ separated_pickle.py
@@ -6,12 +6,16 @@ import re
 from rpy2.robjects import r
 from rpy2.robjects.numpy2ri import numpy2ri
 
+interval = 10
+
 for i in range(1,5):
-    ao = 'results/noH/apo10_sim'+str(i)
-    po = 'results/noH/prfar10_sim'+str(i)
+    ao = 'results/noH/apo_mean'+str(interval)+'_sim'+str(i)
+    po = 'results/noH/prfar_mean'+str(interval)+'_sim'+str(i)
     mat = pkl.load(open('results/noH/sim'+str(i)+'.p', 'rb'))
-    np.save(ao+'.npy', mat[:,:,:1000:10])
-    np.save(po+'.npy', mat[:,:,1000::10])
+    newmat = np.mean(mat.reshape((*mat.shape[:2], int(2000/interval), interval)), axis=-1)
+    np.save(ao+'.npy', newmat[:,:,:100])
+    np.save(po+'.npy', newmat[:,:,100:])
+    
 
 
 
