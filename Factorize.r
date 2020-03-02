baf8ba868c5c4b5c6c5cea1aaf392cf0d646579d
Aria GHEEREART <aria.gheeraert@gmail.com>
2020-03-02 17:10:08 +0100

    new factorization added

--- Factorize.r
+++ Factorize.r
@@ -2,13 +2,13 @@ library(multiway)
 library(reticulate)
 use_python("/home/aghee/anaconda3/bin/python")
 np <- import("numpy")
-X = np$load('results/apo_sim1.npy')
-cmin = 3
-cmax = 7
+X = np$load('results/noH10/mean10_round.py')
+cmin = 21
+cmax = 24
 
 for (R in cmin:cmax){
     pfac <- parafac(X,nfac=R,nstart=20, const = c("nonneg", "nonneg", "nonneg"), output="best")
-    np$save(paste0("results/ABC", R, ".npy"), pfac)
+    np$save(paste0("results/noH10/ABC", R, ".npy"), pfac)
 }
 
 # jpeg("results/core_consistency.jpg", width = 350, height = 350)
