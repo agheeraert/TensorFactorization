859ca3bc867151debc2ed9ca8ef17e2bc5781e63
Aria GHEEREART <aria.gheeraert@gmail.com>
2020-03-02 12:32:05 +0100

    minor modifications

--- core_consistency.r
+++ core_consistency.r
@@ -3,7 +3,7 @@ library(reticulate)
 use_python("/home/aghee/anaconda3/bin/python")
 np <- import("numpy")
 X = np$load('results/noH/apo_sim1.npy')
-cmin = 12
+cmin = 19
 cmax = 20
 nstart = 20
 
--- plot_cc_from_csv.py
+++ plot_cc_from_csv.py
@@ -2,15 +2,15 @@ from numpy import genfromtxt
 import numpy as np
 import matplotlib.pyplot as plt
 
-table = genfromtxt('results/noH/partial_cc.csv', delimiter=',')
+table = genfromtxt('results/partial_cc.csv', delimiter=',')
 best5 = np.sort(table, axis=-1)[:,-5:]
 plt.style.use('classic')
 f = plt.figure()
-plt.plot(range(2,12), np.mean(best5, axis=-1), color='k')
+plt.plot(range(2,best5.shape[0]+2), np.mean(best5, axis=-1), color='k')
 for i in range(5):
-    plt.scatter(range(2,12), best5[:,i], marker='+', color='g')
+    plt.scatter(range(2,best5.shape[0]+2), best5[:,i], marker='+', color='g')
 plt.xlabel("Number of components")
 plt.ylabel("core consistency (%)")
-plt.xlim(1.5,11.5)
+plt.xlim(1.5,best5.shape[0]+1.5)
 plt.ylim(0,100)
-plt.savefig("results/noH/cc_classic.png")
+plt.savefig("results/cc_classic.png")
