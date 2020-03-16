3189009312e744d2bfb862ad44821fa4573235c2
Aria <aria@remus>
2020-03-16 14:07:23 +0100

    tensor modificator utilitary added

--- tensor_modificator.py
+++ tensor_modificator.py
@@ -10,16 +10,25 @@ from Bio.PDB.Polypeptide import aa1, aa3
 import networkx as nx
 import argparse
 import mdtraj as md
+from Tensor import Tensor
 
 parser = argparse.ArgumentParser(description='Operates on ')
-parser.add_argument('f',  type=str, nargs='+',
-                    help='list of trajectories and topology file')
-parser.add_argument('-p',  type=str, 
-                    help='output path')
+parser.add_argument('-f',  type=str, nargs='+',
+                    help='List of path of tensors to concatenate and modify')
+parser.add_argument('-w', type=int, nargs=1, default=10, 
+                    help='time window for perfoming the operations')
+parser.add_argument('-a',  type=str, default='all',
+                    help='actions to perform')
 parser.add_argument('-o',  type=str, default='results/diff.png',
                     help='output path')
 args = parser.parse_args()
 
-if 'all' in args.p:
+ts = Tensor(args.f)
+ts.set_window(args.w)
+if args.a == 'all':
+    ts.all_operations(args.o)
+
+
+
     
 
